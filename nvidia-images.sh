1badc6202f984ba013ceac1c8e3d3a9c9a6dc6b8
crslen <lennonc@gmail.com>
2025-05-15 14:07:58 +0000

    Fixed llm download issues

--- nvidia-images.sh
+++ nvidia-images.sh
@@ -6,18 +6,26 @@ mapfile -t helm_charts < <(jq -r '.helm[]' './config/images.json')
 mapfile -t llm < <(jq -c '.models.llm[]' './config/images.json')
 mapfile -t embedding < <(jq -c '.models.embedding[]' './config/images.json')
 
+# update docker to ignore harbor self-signed cert
 sudo jq ". += {\"insecure-registries\":[\"${BOOTSTRAP_REGISTRY}\"]}" /etc/docker/daemon.json > /tmp/temp.json && sudo mv /tmp/temp.json /etc/docker/daemon.json
 sudo systemctl restart docker
 
+# update certificate to ignore hrbor self-signed cert
+openssl s_client -showcerts -servername $BOOTSTRAP_REGISTRY -connect $BOOTSTRAP_REGISTRY:443 </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ./certificates/$BOOTSTRAP_REGISTRY.crt
+sudo cp ./certificates/$BOOTSTRAP_REGISTRY.crt /usr/local/share/ca-certificates/$BOOTSTRAP_REGISTRY.crt
+sudo update-ca-certificates
 
 docker login ${BOOTSTRAP_REGISTRY} -u "$BOOTSTRAP_REGISTRY_USERNAME" -p "$BOOTSTRAP_REGISTRY_PASSWORD"
 docker login nvcr.io -u '$oauthtoken' -p "$NGC_API_KEY"
 
+# pull images using docker
 for image in "${container_images[@]}"; do
-    echo "==> Start to push container image: $image"
+    echo "==> Start to pull container image: $image"
     version=$(echo "$image" | sed "s/^[^/]*\//$BOOTSTRAP_REGISTRY\/$BOOTSTRAP_NVIDIA_REPO\//")
     docker pull $image
+    # tanzu imgpkg copy -b "$image" --to-tar "$DOWNLOAD_DIR_TAR"/"$file_name".tar --cosign-signatures
     docker tag $image $version
+    echo "==> Start to push container image: $version"
     docker push $version
 done
 
@@ -50,6 +58,20 @@ for m in "${llm[@]}"; do
     done
 done
 
+# Embedding model profiles
+emb_output=()
+for m in "${embedding[@]}"; do
+    name=$(echo "$m" | jq -r '.name')
+    uri=$(echo "$m" | jq -r '.uri')
+
+    profiles=$(echo "$m" | jq -c '.profiles[]')
+    for profile in $profiles; do
+        profile_name=$(echo "$profile" | jq -r '.profile_name')
+        profile_id=$(echo "$profile" | jq -r '.profile_id')
+        emb_output+=("$name, $uri, $profile_name, $profile_id")
+    done
+done
+
 # Pull all LLM model files
 for item in "${llm_output[@]}"; do
     IFS=', ' read -r image_name uri profile_name profile_id <<< "$item"
@@ -74,10 +96,11 @@ done
 
 # Push LLM to bootstrap harbor
 # Push LLM files.
+working_dir=$(pwd)
 for item in "${llm_output[@]}"; do
     IFS=', ' read -r image_name uri profile_name profile_id <<< "$item"
 
-    local_model_store_path="$BASTION_RESOURCES_DIR/$image_name/$profile_name"_model
+    local_model_store_path="$working_dir/$BASTION_RESOURCES_DIR/$image_name/$profile_name"_model
 
     if [[ ! -d "$local_model_store_path" ]]; then
         echo "File not found: $local_model_store_path"
@@ -85,16 +108,20 @@ for item in "${llm_output[@]}"; do
     fi
     cd "$local_model_store_path" || exit
     echo "==> Pushing model: $local_model_store_path to model store: \
-        $LOCAL_HARBOR_URI/model-store/$image_name/$profile_name"
-    pais models push --modelName "models/$image_name/$profile_name" --modelStore "$BOOTSTRAP_REGISTRY" -t v1
+        $BOOTSTRAP_REGISTRY/model-store/$image_name/$profile_name"
+    pais models push --modelName "$image_name/$profile_name" --modelStore "$BOOTSTRAP_REGISTRY/model-store" -t v1
 done
 
 # Pull all embedding model files
+cd $working_dir
 for item in "${emb_output[@]}"; do
     IFS=', ' read -r image_name uri profile_name profile_id <<< "$item"
 
     local_model_cache_path="$BASTION_RESOURCES_DIR/$image_name/$profile_name"_cache
     local_model_store_path="$BASTION_RESOURCES_DIR/$image_name/$profile_name"_model
+
+    mkdir -p $local_model_cache_path
+    mkdir -p $local_model_store_path
 
     echo "==> Pulling model: $image_name profile: $profile_name to $local_model_cache_path"
     docker run -it --rm --name="$image_name" \
@@ -102,7 +129,7 @@ for item in "${emb_output[@]}"; do
         -v "$local_model_store_path":/model-repo \
         -e NGC_API_KEY="$NGC_API_KEY" \
         $( [ -n "$HTTP_PROXY" ] && [ -n "$HTTPS_PROXY" ] && echo " -e http_proxy=$HTTP_PROXY -e https_proxy=$HTTPS_PROXY -e no_proxy=$NO_PROXY" ) \
-        -u "$(id -u root)" \
+        -u "$(id -u)" \
         "$uri" \
         bash -c "download-to-cache --profile $profile_id"
 
@@ -112,9 +139,9 @@ for item in "${emb_output[@]}"; do
 
     # tar all embedding model files
     path="$BASTION_RESOURCES_DIR/$profile_name.tgz"
-    tar -czvf $path -C $local_model_cache_path
-    mkdir -p "$BASTION_RESOURCES_DIR/$profile_name"
-    mv $path "$BASTION_RESOURCES_DIR/$profile_name"
+    tar -czvf $path $local_model_cache_path
+    mkdir -p "$BASTION_RESOURCES_DIR/$image_name/$profile_name"
+    mv $path "$BASTION_RESOURCES_DIR/$image_name/$profile_name"
     echo "Archived: $path"
 done
 	
@@ -122,7 +149,7 @@ done
 for item in "${emb_output[@]}"; do
     IFS=', ' read -r image_name uri profile_name profile_id <<< "$item"
 
-    local local_model_store_path="$BASTION_RESOURCES_DIR/$image_name/$profile_name"
+    local_model_store_path="$working_dir/$BASTION_RESOURCES_DIR/$image_name/$profile_name"
 
     if [[ ! -d "$local_model_store_path" ]]; then
         echo "File not found: $local_model_store_path"
@@ -131,7 +158,7 @@ for item in "${emb_output[@]}"; do
     cd "$local_model_store_path" || exit
     echo "==> Pushing model: $local_model_store_path  to model store: \
         $BOOTSTRAP_REGISTRY/model-store/$image_name/$profile_name"
-    pais models push --modelName "model-store/$image_name/$profile_name" --modelStore "$BOOTSTRAP_REGISTRY" -t v1
+    pais models push --modelName "$image_name/$profile_name" --modelStore "$BOOTSTRAP_REGISTRY/model-store" -t v1
 done
 
-## todo - add push llm and embed (need pais)
+# todo - add push llm and embed (need pais)
